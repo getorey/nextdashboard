d407feaeaa457ed6935a80d7c770d2964b203826
kimjieun <getorey@gmail.com>
2024-04-01 18:08:22 +0900

    .

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Page() {
         alt="Screenshot of the dashboard project showing mobile version"/>
         </div>
       </div>
-      <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"s/>
+      <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/>
     </main>
   );
 }
