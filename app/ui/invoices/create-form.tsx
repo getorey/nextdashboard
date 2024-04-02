7cb6f6f58fb3d62ccca39c1fa12519a95d4e6a61
kimjieun <getorey@gmail.com>
2024-04-02 21:38:24 +0900

    .

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -18,7 +18,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 
 
   return (
-    <form action={ createInvoice }>
+    <form action={ dispatch }>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
